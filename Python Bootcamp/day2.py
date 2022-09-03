071c6a21a0494486521155c05bda169e167497b3
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-03 11:07:43 +0000

    challenge 3

--- Python Bootcamp/day2.py
+++ Python Bootcamp/day2.py
@@ -22,21 +22,45 @@
 ###############################################################
 # Challenge 2
 ###############################################################
+# # 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+
+# # Convert the string to a float
+# height_num = float(height)
+# weight_num = float(weight)
+
+# # Use the BMI calculation
+# BMI = weight_num / (height_num ** 2)
+
+# # Use format to round of the float output to 2 decimal number
+# print("Your BMI is " + format(BMI, '.2f'))
+# # Convert int to str to concatenate to str
+# print("Your BMI is " + str(round(BMI)))
+
+###############################################################
+# Challenge 3
+###############################################################
 # 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+new_age = int(age)
 
-# Convert the string to a float
-height_num = float(height)
-weight_num = float(weight)
+total_days = 90 * 365
+total_weeks = 90 * 52
+total_months = 90 * 12
 
-# Use the BMI calculation
-BMI = weight_num / (height_num ** 2)
+consumed_days = new_age * 365
+consumed_weeks = new_age * 52
+consumed_months = new_age *12
 
-# Use format to round of the float output to 2 decimal number
-print("Your BMI is " + format(BMI, '.2f'))
-# Convert int to str to concatenate to str
-print("Your BMI is " + str(round(BMI)))
+days_left = total_days - consumed_days
+weeks_left = total_weeks - consumed_weeks
+months_left = total_months - consumed_months
+
+print(f"You have\n {days_left} days,\n {weeks_left} weeks, and\n {months_left} months left.")

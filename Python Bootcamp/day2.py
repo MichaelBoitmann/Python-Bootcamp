62ca4d4730e815747ee9b691e189f4db4ad54e70
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-03 12:47:34 +0000

    rearrange the last two output

--- Python Bootcamp/day2.py
+++ Python Bootcamp/day2.py
@@ -12,7 +12,7 @@
 # two_digit_number = input("Type a two digit number: ")
 # # Input digits are in string type
 # print(type(two_digit_number))
-# # Subcripting each digit to separate 
+# # Subcripting each digit to separate
 # first_digit = two_digit_number[0]
 # second_digit = two_digit_number[1]
 # # Convert each digit from string to int
@@ -72,7 +72,8 @@ print("Welcome to the tip calculator.")
 
 total_bill = float(input("What was the total bill? $"))
 
-percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
+percentage = input(
+    "What percentage tip would you like to give? 10, 12, or 15? ")
 
 tip_amount = format((int(total_bill) * (int(percentage) / 100)), '.2f')
 
@@ -80,10 +81,11 @@ convert_percentage = float(1 + (int(percentage) / 100))
 
 num_people = int(input("How many people to split the bill? "))
 
-bill_distribution = format((total_bill * convert_percentage) / num_people, '.2f')
+bill_distribution = format((total_bill * convert_percentage) / num_people,
+                           '.2f')
 
 print("\n\nYour Receipt")
 print(f"Total Bill: ${format(total_bill, '.2f')}")
 print(f"Selected Percentage: {percentage}%")
 print(f"Number of People: {num_people}\n")
-print(f"Each person will contribute a total of ${bill_distribution} to give a tip of amount of ${tip_amount}")
+print(f"Each person should pay ${bill_distribution}\nTip Amount: ${tip_amount}")

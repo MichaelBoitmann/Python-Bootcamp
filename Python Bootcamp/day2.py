cd160c2e8aa6c09ea9f45531cb10de94521d575e
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-03 12:41:18 +0000

    some changes on the total bill format and converting the given variables type

--- Python Bootcamp/day2.py
+++ Python Bootcamp/day2.py
@@ -70,20 +70,20 @@
 ###############################################################
 print("Welcome to the tip calculator.")
 
-total_bill = input("What was the total bill? $")
+total_bill = float(input("What was the total bill? $"))
 
 percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
 
 tip_amount = format((int(total_bill) * (int(percentage) / 100)), '.2f')
 
-convert_percentage = 1 + (int(percentage) / 100)
+convert_percentage = float(1 + (int(percentage) / 100))
 
-num_people = input("How many people to split the bill? ")
+num_people = int(input("How many people to split the bill? "))
 
-bill_distribution = format((float(total_bill) * float(convert_percentage)) / int(num_people), '.2f')
+bill_distribution = format((total_bill * convert_percentage) / num_people, '.2f')
 
 print("\n\nYour Receipt")
-print(f"Total Bill: ${total_bill}")
-print(f"Selected Percentage: {percentage}")
+print(f"Total Bill: ${format(total_bill, '.2f')}")
+print(f"Selected Percentage: {percentage}%")
 print(f"Number of People: {num_people}\n")
 print(f"Each person will contribute a total of ${bill_distribution} to give a tip of amount of ${tip_amount}")

c2ca4df4b15343660b7d818d1fca04f265edd22f
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-05 10:43:33 +0000

    leap year challenge

--- Python Bootcamp/day3.py
+++ Python Bootcamp/day3.py
@@ -19,8 +19,10 @@ else:
 ###############################################################
 # Challenge 1 = Odd or Even
 ###############################################################
+# 🚨 Don't change the code below 👇
 print("Let me guess your number if odd or even")
 number = int(input("Please key-in any number: "))
+# 🚨 Don't change the code above 👆
 
 if number % 2 == 0:
 	print(f"{number} is Even")
@@ -31,15 +33,44 @@ else:
 # Challenge 2 = BMI 2.0
 ###############################################################
 
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
 BMI = round(weight / height ** 2)
 
 if BMI < 18.5:
-    print(f"You BMI is {BMI}, you are underweight.")
+    print(f"Your BMI is {BMI}, you are underweight.")
 elif BMI < 25:
-    print(f"You BMI is {BMI}, you are normal weight.")
+    print(f"Your BMI is {BMI}, you have a normal weight.")
 elif BMI < 30:
-    print(f"You BMI is {BMI}, you are slightly overweight.")
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
 elif BMI < 35:
-    print(f"You BMI is {BMI}, you are obese.")
+    print(f"Your BMI is {BMI}, you are obese.")
 else:
-    print(f"You BMI is {BMI}, you are clinically obese.")
+    print(f"Your BMI is {BMI}, you are clinically obese.")
+
+
+###############################################################
+# Challenge 3 = Leap Year
+###############################################################
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")

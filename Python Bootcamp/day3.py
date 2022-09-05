6ba956dcdbfbee7eae6e2d4d2e1de5502fb8239f
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-05 06:01:16 +0000

    started day 3 challenge

---
+++ Python Bootcamp/day3.py
@@ -0,0 +1,22 @@
+###############################################################
+# Lecture for if/else statement
+###############################################################
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else: 
+#     print("Sorry, you have to grow taller before you can ride.")
+
+###############################################################
+# Challenge 1 = Odd or Even
+###############################################################
+print("Let me guess your number if odd or even")
+number = int(input("Please key-in any number: "))
+
+
+if number % 5 == 0:
+    print(f"{number} is Even")
+else:
+    print(f"{number} is Odd")
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-print("Welcome to the tip calculator.")
+print("Let me guess your number if odd or even")
+number = int(input("Please key-in any number: "))
 
-total_bill = float(input("What was the total bill? $"))
 
-percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
-
-tip_amount = format((int(total_bill) * (int(percentage) / 100)), '.2f')
-
-convert_percentage = float(1 + (int(percentage) / 100))
-
-num_people = int(input("How many people to split the bill? "))
-
-bill_distribution = format((total_bill * convert_percentage) / num_people, '.2f')
-
-print("\n\nYour Receipt")
-print(f"Total Bill: ${format(total_bill, '.2f')}")
-print(f"Selected Percentage: {percentage}%")
-print(f"Number of People: {num_people}\n")
-print(f"Each person should pay ${bill_distribution}\nTip Amount: ${tip_amount}")
+if number % 2 == 0:
+    print(f"{number} is Even")
+else:
+    print(f"{number} is Odd")

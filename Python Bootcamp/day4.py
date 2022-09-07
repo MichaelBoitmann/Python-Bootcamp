3aadcc0c9124a7d0f7322c317c1958f1115a30c0
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-07 11:26:52 +0000

    challenge 1 heads or tails

--- Python Bootcamp/day4.py
+++ Python Bootcamp/day4.py
@@ -8,3 +8,15 @@ print(random_integer)
 
 random_float = random.random()
 print(format((random_float * 100), '.2f'))
+
+###############################################################
+# Challenge 1 - Heads or Tails
+###############################################################
+import random
+
+random_side = random.randint(0, 1)
+
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")

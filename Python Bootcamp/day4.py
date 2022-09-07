173d38a86059e31dedca2a461d3d3d564e462532
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-07 11:43:06 +0000

    additional lecture

--- Python Bootcamp/day4.py
+++ Python Bootcamp/day4.py
@@ -9,6 +9,12 @@ print(random_integer)
 random_float = random.random()
 print(format((random_float * 100), '.2f'))
 
+randomFloat = random.random() * 5
+print("randomFloat")
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+
 ###############################################################
 # Challenge 1 - Heads or Tails
 ###############################################################
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random
 
-random_side = random.randint(0, 1)
+random_integer = random.randint(1, 1000)
+print(random_integer)
 
-if random_side == 1:
-    print("Heads")
-else:
-    print("Tails")
+random_float = random.random()
+print(format((random_float * 100), '.2f'))
+
+randomFloat = random.random() * 5
+print("randomFloat")
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+

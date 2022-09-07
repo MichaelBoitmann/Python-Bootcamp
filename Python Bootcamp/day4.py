2cb8acae979ee6c98c0d625f04e188fa1a230480
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-07 11:06:07 +0000

    day 4 lecture

---
+++ Python Bootcamp/day4.py
@@ -0,0 +1,10 @@
+###############################################################
+# Lecture for random / randint() / random()
+###############################################################
+import random
+
+random_integer = random.randint(1, 1000)
+print(random_integer)
+
+random_float = random.random()
+print(format((random_float * 100), '.2f'))

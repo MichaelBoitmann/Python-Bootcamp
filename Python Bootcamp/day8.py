a415afaafc50b00ff9e15d1b249fceced125319d
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-18 09:36:37 +0000

    day8 with new task

--- Python Bootcamp/day8.py
+++ Python Bootcamp/day8.py
@@ -5,7 +5,9 @@ def greet(desc1, desc2):
 # Write 3 print statements inside the function.
     print("Hello Pythonians!")
     print("I am looking forward to be good in Pyhton!")
-    print(f"I just need a great {desc1} and {desc2}!")
+    print("I just need a great {desc1} and deeper {desc2}")
     
 # Call the greet() function and run your code.
 greet("encouragement", "passion")
+
+

a89320f356e20a277ca3fe880ff2d112b1adbb54
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-19 08:23:24 +0000

    day8 challenge for Pain Area Calculator

--- Python Bootcamp/day8.py
+++ Python Bootcamp/day8.py
@@ -14,6 +14,8 @@ greet("encouragement", "passion", "Pythonians")
 
 
 ###########################################################################
+# Paint Area Calculator
+###########################################################################
 
 #Write your code below this line 👇
 import math
@@ -21,18 +23,12 @@ import math
 def paint_calc(height, width, cover):
     area = height * width
     num_of_cans = math.ceil(area / cover)
-    print(f"You'll need {num_of_cans} cans of paint.")
-
-
-
-
-
-
-#Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.   
+    print(f"You'll need {num_of_cans} cans of paint.")   
 
 # 🚨 Don't change the code below 👇
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
-coverage = 5
+coverage = int(input("Number of Coverage: "))
+
+# Calling out the function
 paint_calc(height=test_h, width=test_w, cover=coverage)

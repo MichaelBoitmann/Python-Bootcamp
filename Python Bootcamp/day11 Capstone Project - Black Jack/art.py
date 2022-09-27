e8d7a1dadfca1d2fa217e498e203466285a5fe88
Michael Boitmann <55775047+MichaelBoitmann@users.noreply.github.com>
2022-09-27 08:56:46 +0000

    final touch of Black Jack as the first Capstone Project

---
+++ Python Bootcamp/day11 Capstone Project - Black Jack/art.py
@@ -0,0 +1,48 @@
+Skip to content
+
+appbrewery
+@appbrewery/blackjack-final
+blackjack-final
+Want to join the course? Start your journey here: https://100daysofpython.dev/
+Open on Replit
+
+Hide files
+
+
+424
+
+Run
+128.5K
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+                   
+
+                                      
+     
+
+Files
+
+blackjack-final - Replit

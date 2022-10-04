bf2091e3e7733194109bb194f00a062b353566ef
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-10-04 22:39:02 +1300

    completed coffee machine challenge with oop in additon!

--- main.py
+++
@@ -1,7 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtyle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-
-print(table)
